6a42a06d0dfc15a23455cbc91dd9c20f6fcff5f6
KamlaX <40739398+KamlaXPL@users.noreply.github.com>
2020-02-27 22:02:27 +0100

    Add files via upload

--- src/main/java/me/kamlax/spectrum/Main.java
+++ src/main/java/me/kamlax/spectrum/Main.java
@@ -7,13 +7,9 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author KamlaX on 27.02.2020
  */
 public class Main extends JavaPlugin {
-    public Main() {
-        Main.plugin = this;
-    }
-    private static Main plugin;
+
     @Override
     public void onEnable() {
-        Main.plugin = this;
         getServer().getPluginManager().registerEvents( new SpectrumListener(), this);
     }
 }
