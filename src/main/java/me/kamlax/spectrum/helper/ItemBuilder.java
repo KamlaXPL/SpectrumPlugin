16ca4cb56b9e3adda9ac357765720ea72002c19c
KamlaX <40739398+KamlaXPL@users.noreply.github.com>
2020-02-29 13:28:47 +0100

    Add files via upload

--- src/main/java/me/kamlax/spectrum/helper/ItemBuilder.java
+++ src/main/java/me/kamlax/spectrum/helper/ItemBuilder.java
@@ -15,10 +15,6 @@ public class ItemBuilder {
         this.itemStack = itemStack;
     }
 
-    public static ItemBuilder of(ItemStack itemStack) {
-        return new ItemBuilder(itemStack);
-    }
-
     public ItemBuilder addEnchant(Enchantment enchantment, int level) {
         itemStack.addEnchantment(enchantment, level);
         return this;
